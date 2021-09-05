c097354c40266ffcf5091fce2a0cb27edb17aa7b
thanapat <thnanap_2593@hotmail.com>
2021-09-05 23:27:30 +0700

    comment

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">  
+    <!-- **navbar** -->
+      <ul>
+        <li ><a href="#home"><h1 style="font-size:30px">THANAPAT SEKAEW</h1></a></li>
+        <li style="float:right"><a href="#experience"><h1 style="font-size:20px">EXPERIENCE</h1></a></li>
+        <li style="float:right"><a href="#profile"><h1 style="font-size:20px">PROFILE</h1></a></li>
+        <li style="float:right"><a href="#skill"><h1 style="font-size:20px">SKILL</h1></a></li>
+      </ul>
+      <!-- **end-navbar** -->
+
+      <!-- **image profile** -->
+            <div class="one">
+            <center>
+                <img src="./assets/profile_resume.png" style="vertical-align:middle">
+                <h1>THANAPAT SEKAEW</h1>
+                <img src="./assets/iconcom.png" width="150" height="100">
+                <h1>WEB DEVELOPER</h1>
+            </center>
+      <!-- **end-image profile** -->
+
+          
+  </div>
+  <!-- **Skill profile** -->
+            <section id="skill">
+            <div>
+              <center><h1>Skill</h1></center>
+              <img src="./assets/css.png" width="100" height="100">
+              <img src="./assets/appserv.jpg" width="100" height="100">
+              <img src="./assets/javascript.png" width="100" height="100">
+              <img src="./assets/php.jpg" width="100" height="100">
+              <img src="./assets/yii2.png" width="100" height="100">
+              <img src="./assets/mysql.png" width="100" height="100">
+            </div>
+            </section>
+            <section id="profile">
+            <div>
+              <center><h1>PROFILE</h1></center>
+              <h1>Name : Thanapat Sekaew</h1>
+              <h1>Nickname : Aof</h1>
+              <h1>Age : 25</h1>
+              <h1>Facebook : Thanapat Sekaew</h1>
+              <h1>Line : Aofslacken(0846982979)</h1>
+            </div>
+            </section>
+            <section id="experience">
+            <div>
+              <center><h1>EXPERIENCE</h1></center>
+            </div>
+            </section>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
